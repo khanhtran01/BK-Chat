3b9ae9e8972d307959bb26c975d206f1317dc156
Trần Lê Viết Khánh <65809244+khanhtran01@users.noreply.github.com>
2022-10-10 15:12:58 +0700

    format

--- server/src/index.js
+++ server/src/index.js
@@ -1,32 +1,32 @@
-require('dotenv').config()
-const express = require('express');
-const morgan = require('morgan');
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 4000;
-const route = require('./routes');
-const cookieParse = require('cookie-parser');
+const route = require("./routes");
+const cookieParse = require("cookie-parser");
 const http = require("http");
 const server = http.createServer(app);
 
-const db = require('./config/db/index');
+const db = require("./config/db/index");
 
 db.connect();
 // Middlleware built-in
 app.use(
-    express.urlencoded({
-        extended: true,
-    }),
+  express.urlencoded({
+    extended: true,
+  })
 ); // unicode
 app.use(express.json()); // axios, ajax
 app.use(cookieParse(process.env.COOKIE_SECRECT)); //cookie parser
 
 // Set up
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 app.use(cors());
 
 // Routes init
 route(app);
 server.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`);
+  console.log(`App listening at http://localhost:${port}`);
 });
