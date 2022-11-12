c473c3812c20c5ad3d8b7b9bf2817e2970614ea6
Trần Lê Viết Khánh <65809244+khanhtran01@users.noreply.github.com>
2022-11-12 22:08:30 +0700

    caculate reply rate between 2 users

--- nlp/src/functions/suggestion/index.js
+++ nlp/src/functions/suggestion/index.js
@@ -71,14 +71,56 @@ connect();
 //         console.log(error);
 //     }
 // })
-function countReply(uidA, uidB){
-    let count = 0;
-}
 
+/**
+ * TODO: caculate the friendly ratio between 2 users
+ * @param {Object} data list of chat in conversation
+ * @param {String} uidA id of user
+ * @param {String} uidB id of user
+ * @param {Number} avg average dentaTime of all users
+ * @returns {Object} friendly ratio between 2 users
+ */
+function calcReplyRate(data ,uidA, uidB, avg){
+    let basisCount = 0; // count of reply
+    let advantageCount = 0; // count of little dentaTime 
+    let connectCount = 0;
+    let currTime, oldTime, currId, oldId = null;
+    data.map(chat => {
+        if (chat.uid === uidA || chat.uid === uidB ) {
+            currTime = chat.time;
+            currId = chat.uid;
+            if ((chat.uid === uidA && chat.replyFrom === uidB) || (chat.uid === uidB && chat.replyFrom === uidA)) {
+                connectCount++;
+                basisCount++;
+            } else if (oldId != null && currId !== oldId ){
+                connectCount++;
+                if ((currTime.getTime() - oldTime.getTime())/1000 <= avg*0.2 && chat.replyFrom === null){
+                    advantageCount++;
+                }
+            }
+            oldTime = currTime;
+            oldId = currId;
+        }
+    })
+    // return dentaTime;
+    // console.log("reply rate :" + (basisCount + advantageCount) / connectCount );
+    return (basisCount + advantageCount) / connectCount;
+}
+/**
+ * TODO: caculate sum of all element in array
+ * @param {Object} data array of elements
+ * @returns {Number} sum of all elements in array
+ */
 function getSumFromArray(data){
     return data.reduce((partialSum ,a) => partialSum + a, 0)
 }
-
+/**
+ * TODO: get list denta time of all users, this function can use to get dentaTime of 2 users
+ * @warning 
+ * ! This result demands on fix time is 6 hours
+ * @param {Object} id
+ * @returns {Object} list of chat
+ */
 function getDentaTimeList(data, idA = "", idB = "") {
     let currTime, oldTime, currId, oldId = null;
     let dentaTime = [];
@@ -86,6 +128,7 @@ function getDentaTimeList(data, idA = "", idB = "") {
         if (chat.uid === idA || chat.uid === idB || (idA === "" && idB === "")) {
             currTime = chat.time;
             currId = chat.uid;
+            //* if dentaTime over 6 hours will be skip
             if (oldId != null && currId !== oldId && (currTime.getTime() - oldTime.getTime()) <= 6 * 60 * 60 * 1000){
                 dentaTime.push((currTime.getTime() - oldTime.getTime())/1000);
             }
@@ -95,8 +138,12 @@ function getDentaTimeList(data, idA = "", idB = "") {
     })
     return dentaTime;
 }
-
-async function getALlMessage(){
+/**
+ * TODO: get all chat of conversationID
+ * @param {Object} id
+ * @returns {Object} list of chat
+ */
+async function getAllMessage(id,rgg){
     let listMessages = await Chat.find({})
     let newList = [];
     listMessages.map(message => {
@@ -106,18 +153,18 @@ async function getALlMessage(){
             replyFrom: message.replyFrom,
         })
     })
-    const A_B = getDentaTimeList(newList, '383935313139303832616161', '393734363638393835616161' )
-    console.log('=============AVG TIME OF A AND B=======================');
-    console.log(getSumFromArray(A_B)/A_B.length);
-    console.log('====================================');
-    const all = getDentaTimeList(newList)
-    console.log('=============AVG TIME OF ALL USER================');
-    console.log(getSumFromArray(all)/all.length);
-    console.log('====================================');
+    return newList;
 }
+async function todo(){
+    // data of one conversation
+    const data = await getAllMessage();
+    const dentaTime = getDentaTimeList(data);
+    const avgTime = getSumFromArray(dentaTime)/dentaTime.length;
+    const replyRate = calcReplyRate(data, '383935313139303832616161', '333833373631353439616161', avgTime);
+    console.log("reply rate: " + replyRate);
 
-getALlMessage();
-
+}
+todo();
 server.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
 });
