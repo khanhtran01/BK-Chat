6dcd66f611c7d74ec9b9791264e4ef1e629a83df
Trần Lê Viết Khánh <65809244+khanhtran01@users.noreply.github.com>
2022-11-14 22:01:30 +0700

    predict all group maybe split

--- nlp/src/functions/suggestion/index.js
+++ nlp/src/functions/suggestion/index.js
@@ -3,11 +3,13 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3500;
 const mongoose = require('mongoose');
+const moment = require('moment');
 const http = require("http");
 const server = http.createServer(app);
 const Account = require('../../models/Account')
 const Conversation = require('../../models/Conversation')
 const Chat = require('../../models/Chat')
+const fs = require('fs');
 
 async function connect() {
     try {
@@ -23,54 +25,111 @@ async function connect() {
 
 connect();
 
-// function concatChat(chat1, chat2) {
-//     return chat1.concat(" ", chat2);
-// }
+// https://gist.github.com/axelpale/3118596?permalink_comment_id=3888711#gistcomment-3888711
+function combinations(arr, k){
+    let set = [...arr];
+    if (k > set.length || k <= 0) return [];
+    if (k === set.length) return [set];
+  
+    return set.reduce((p, c, i) => {
+      combinations(set.slice(i + 1), k - 1)
+        .forEach(tailArray => p.push([c].concat(tailArray)));
+      return p;
+    }, []);
+}
 
-// async function suggestionEachConversation(conversation) {
-//     let classifies = []
-//     conversation.member.forEach((memberId) => {
-//         var chats = await Chat.find({
-//             conversationId: conversation._id,
-//             userId: memberId
-//         }, { userRead: 0 })
-//             .sort({ 'createdAt': -1 })
-//             .limit(10)
-//         let totalData = "";
-//         chats.forEach((chat) => {
-//             totalData = concatChat(totalData, chat.content);
-//         })
-//         // console.log(getClassifies(totalData));  // array of suggestions
-//     })
-// }
+function checkConnection(data, acceptList) {
 
-// async function suggestionAllConversation() {
-//     try {
-//         const conversations = await Conversation.find({
-//             'type': 'group',
-//             '$expr': { $gte: [{ $size: '$member' }, 5] }
-//         }, { desc: 0, avatar: 0 })
-//         for (const conversation of conversations) {
-//             suggestionEachConversation(conversation)
-//         }
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
-
-// suggestionAllConversation()
-
-// getClassifies("A class should have only a single responsibility. Only one potential change in the software's specification should be able to affect the specification of the class.")
-// getClassifies("That actor on TV makes movies in Hollywood and also stars in a variety of popular new TV shows.")
+    for (let i = 0; i < data.length; i++) {
+        let sum = 0;
+        acceptList.forEach(idx => {
+            sum += data[i][idx];
+        })
+        if (sum < acceptList.length) {
+            return false;
+        }
+    }
+    return true;
+}
 
 
-// app.get('/api/nlp', async (req, res) => {
-//     try {
-//         res.status(200).json() 
-//     } catch (error) {
-//         console.log(error);
-//     }
-// })
+/**
+ * TODO convert path to 2d array with index is length of path and elements is array of uid
+ * @param {Array} path
+ * @return {Array[Array]}
+ */
+function convertToLengthArray(path) {
+    // find max length of path
+    let maxLenght = 0;
+    Object.keys(path).map(uid => {
+        if (path[`${uid}`].length > maxLenght) {
+            maxLenght = path[`${uid}`].length;
+        }
+    })
+
+    let result = new Array(maxLenght + 1).fill([]);
+    let i = 0;
+    Object.keys(path).map(uid => {
+        result[path[`${uid}`].length] = [...result[path[`${uid}`].length], i];
+        i++;
+    })
+    // console.log(result[5]);
+    return result;
+
+}
+
+function convertToBinaryMap(data) {
+    const keysList = Object.keys(data);
+    const result = [];
+    for (let i = 0; i < keysList.length; i++) {
+        let temp = [];
+        keysList.map(uid => {
+            if (data[`${keysList[i]}`].includes(uid) || keysList[i] == uid){
+                temp.push(1);
+            } else {
+                temp.push(0);
+            }
+        })
+        result.push(temp);
+    }
+    return result;
+}
+
+function getPath(data){
+    const results = {};
+    data.map(relationship => {
+        if (results[`${relationship[0]}`] === undefined){
+            results[`${relationship[0]}`] = [relationship[1]];
+        } else {
+            results[`${relationship[0]}`].push(relationship[1]);
+        }
+
+        if (results[`${relationship[1]}`] === undefined){
+            results[`${relationship[1]}`] = [relationship[0]];
+        } else {
+            results[`${relationship[1]}`].push(relationship[0]);
+        }
+    })
+    return results;
+}
+
+function userPairing(userList){
+    const result = [];
+    for(let i = 0 ; i < userList.length - 1; i++) {
+        for(let j = i + 1 ; j < userList.length; j++) {
+            result.push([userList[i], userList[j]]);
+        }
+    }
+    return result;
+}
+
+function getUserIDList(data) {
+    const userList = {}
+    data.map(user => {
+        userList[`${user.uid}`] = true;
+    })
+    return Object.keys(userList);
+}
 
 /**
  * TODO: caculate the friendly ratio between 2 users
@@ -94,7 +153,7 @@ function calcReplyRate(data ,uidA, uidB, avg){
                 basisCount++;
             } else if (oldId != null && currId !== oldId ){
                 connectCount++;
-                if ((currTime.getTime() - oldTime.getTime())/1000 <= avg*0.2 && chat.replyFrom === null){
+                if ((currTime.getTime() - oldTime.getTime())/1000 <= avg*0.5 && chat.replyFrom === null){
                     advantageCount++;
                 }
             }
@@ -143,8 +202,13 @@ function getDentaTimeList(data, idA = "", idB = "") {
  * @param {Object} id
  * @returns {Object} list of chat
  */
-async function getAllMessage(id,rgg){
-    let listMessages = await Chat.find({})
+async function getAllMessage(conversationId, backToDays){
+    let listMessages = await Chat.find({
+        conversationId: conversationId,
+        createdAt: {
+            $gte:moment().subtract(backToDays,'days').utc(false).startOf("day"),
+        }
+    })
     let newList = [];
     listMessages.map(message => {
         newList.push({
@@ -156,15 +220,74 @@ async function getAllMessage(id,rgg){
     return newList;
 }
 async function todo(){
-    // data of one conversation
-    const data = await getAllMessage();
+    // get data of 7 days of conversation id 6344e91b89558fb2b5ec1234
+    const data = await getAllMessage("6344e91b89558fb2b5ec1234", 15);
+    // get dentaTime list of this data
     const dentaTime = getDentaTimeList(data);
+    // caculate average time from denta time list
     const avgTime = getSumFromArray(dentaTime)/dentaTime.length;
-    const replyRate = calcReplyRate(data, '383935313139303832616161', '333833373631353439616161', avgTime);
-    console.log("reply rate: " + replyRate);
+    // get all paring between 2 users
+    const paringList = userPairing(getUserIDList(data));
+    // all paring with > 20% reply Rate
+    const filterParingList = [];
 
+    for (let i = 0; i < paringList.length; i++) {
+        let replyRate = calcReplyRate(data, paringList[i][0], paringList[i][1], avgTime);
+        if (replyRate > 0.2) {
+            paringList[i].push(replyRate);
+            filterParingList.push(paringList[i]);
+        }
+    }
+    // !! số thứ tự từ 0-> n dựa vào object.keys(path)
+    const path = getPath(filterParingList); // path : { 'a' : [b,s,g,r,z], 'b' : [a,b,r,h,w,g], ...}
+    const lengthArray = convertToLengthArray(path); // index là độ dài của path, các elements là số thứ tự của các id có độ dài index
+    const biMap = convertToBinaryMap(path);;
+
+
+    // * tìm các tổ hợp dài nhất 
+    // console.log(path);
+    const res = [];
+    for(let i = lengthArray.length - 1; i > 1 ; i--){
+        // console.log("==========================================");
+        // console.log(`i = ${i}`);
+        let rest = lengthArray.slice(i - lengthArray.length);
+        let merged = rest.reduce(function(prev, next) {
+            return prev.concat(next);
+          });
+        // console.log(merged);
+        if (merged.length < i + 1){
+            continue;
+        } else {
+            // console.log(`to hop ${i+1} so lien ket >= ${i}`);
+            let listCombination = combinations(merged, i+1)
+            // console.log(listCombination);
+            let listBit = listCombination.map(combination => {
+                return combination.map(idx => biMap[idx]);
+            })
+            for (let i = 0; i < listCombination.length; i++) {
+                if (checkConnection(listBit[i], listCombination[i])) {
+                    // console.log("***********Congratulations************");
+                    // console.log("output : ");
+                    // console.log(listCombination[i]);
+                    res.push(listCombination[i]);
+                }
+            }
+        }
+        // console.log("==========================================");
+    }
+
+    // console.log(biMap);
+    console.log(res);
+    for (let i = 0; i < res.length; i++) {
+        res[i] = res[i].map(idx => {
+            return Object.keys(path)[idx];
+        })
+    }
+    console.log(res);
+    console.log("done~!");
 }
 todo();
+    
 server.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
 });
