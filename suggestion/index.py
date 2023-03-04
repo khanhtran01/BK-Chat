701066fb800d77f4a939baa38955c7f5f604c265
Trần Lê Viết Khánh <65809244+khanhtran01@users.noreply.github.com>
2023-03-04 16:10:58 +0700

    limit member and message in group suggestion

--- suggestion/index.py
+++ suggestion/index.py
@@ -91,10 +91,10 @@ def testmodel():
         return render_template('testmodel.html')
 
 
-def get_user_list(labels, userList):
+def get_user_list(labels, userList, reject_list):
     group_dict = {}
     for i in range(len(labels)):
-        if (labels[i] == -1):
+        if (labels[i] == -1 or (labels[i] in reject_list)):
             continue
         else:
             if (labels[i] not in group_dict):
@@ -120,11 +120,11 @@ def get_cluster_dict(labels):
     return final_dict
 
 
-def data_processing_for_get_content(cluster, chat_data):
+def data_processing_for_get_content(cluster, chat_data, reject_list):
     result = {}
     count_prev1 = 0
     for i in range(len(cluster)):
-        if (cluster[i] == -1):
+        if ((cluster[i] == -1) or (cluster[i] in reject_list)):
             count_prev1 += 1
             continue
         else:
@@ -133,12 +133,14 @@ def data_processing_for_get_content(cluster, chat_data):
             else:
                 # print("noi")
                 result[cluster[i]] = result[cluster[i]] + ". " + chat_data[i]
-            
+
     return result
-    # for k,v in 
-    
+    # for k,v in
+
+
 def get_classifies(text_content):
-    client = language_v1.LanguageServiceClient.from_service_account_json(os.getenv("KEY_PATH"))
+    client = language_v1.LanguageServiceClient.from_service_account_json(
+        os.getenv("KEY_PATH"))
     type_ = language_v1.Document.Type.PLAIN_TEXT
     language = "en"
     document = {"content": text_content, "type_": type_, "language": language}
@@ -155,10 +157,11 @@ def get_classifies(text_content):
     )
     return response.categories[0].name
 
+
 @app.route('/api/data', methods=['GET'])
 def test():
-    data = {"0": {"name": "/Finance/Investing/Currencies & Foreign Exchange", "userList": ["313837383832373239686868", "343734313433373734686868", "353034313235323634686868", "333630393037353230686868", "343735333633393032686868", "343039373337393334686868", "343532323431353238686868", "353133343633343439686868", "343333303932343335686868", "343730303835383835686868", "343431373134393430686868", "313734363534393437686868", "353030383033373736686868", "343537383832303431686868", "343233313637353738686868", "333432313932383539686868", "343337363136353232686868", "343031303331303434686868", "333930323239313738686868", "343434373136363233686868", "343330363631323339686868", "343533393236393336686868", "343939303839383631686868", "343637383933373832686868", "333935303431303939686868", "343635353939353339686868"]}, 
-    "1": {"name": "/Finance/Investing/Currencies & Foreign Exchange", "userList": ["343031303331303434686868", "343434373136363233686868", "343730303835383835686868", "343537383832303431686868"]}}
+    data = {"0": {"name": "/Finance/Investing/Currencies & Foreign Exchange", "userList": ["313837383832373239686868", "343734313433373734686868", "353034313235323634686868", "333630393037353230686868", "343735333633393032686868", "343039373337393334686868", "343532323431353238686868", "353133343633343439686868", "343333303932343335686868", "343730303835383835686868", "343431373134393430686868", "313734363534393437686868", "353030383033373736686868", "343537383832303431686868", "343233313637353738686868", "333432313932383539686868", "343337363136353232686868", "343031303331303434686868", "333930323239313738686868", "343434373136363233686868", "343330363631323339686868", "343533393236393336686868", "343939303839383631686868", "343637383933373832686868", "333935303431303939686868", "343635353939353339686868"]},
+            "1": {"name": "/Finance/Investing/Currencies & Foreign Exchange", "userList": ["343031303331303434686868", "343434373136363233686868", "343730303835383835686868", "343537383832303431686868"]}}
     requests.post(f'http://localhost:4000/api/notification/new', json={
         'conversationId': '6344e91b89558fb2b5ec0001',
         'data': json.dumps(data)
@@ -167,14 +170,16 @@ def test():
         'successful': True,
     }), status=200, mimetype='application/json')
 
+
 @app.route('/api/checkGrouping', methods=['POST'])
 def checkgrouping():
     conversation_id = request.form['conversation_id']
     datareal = {}
-    data = db.chats.find({'conversationId': conversation_id}).sort("createdAt", -1).limit(100)
-    
-    
+    data = db.chats.find({'conversationId': conversation_id}).sort(
+        "createdAt", -1).limit(1000)
 
+    GROUP_MEMBER_LIMIT = [4, 20]
+    GROUP_MESSAGE_LIMIT = 20
     for e in data:
         datareal[str(e['_id'])] = e
 
@@ -197,29 +202,42 @@ def checkgrouping():
     data_processing = np.array(sentence_embeddings)
     norm_data = normalize(data_processing, norm='l2')
     clusterer = hdbscan.HDBSCAN(algorithm='best', alpha=1.0, approx_min_span_tree=True,
-                                min_cluster_size=3, gen_min_span_tree=True, prediction_data=True)
+                                min_cluster_size=8, gen_min_span_tree=True, prediction_data=True)
     clusterer.fit(norm_data)
-    result = data_processing_for_get_content(np.flip(clusterer.labels_), np.flip(sentences))
-    # json.loads()
-    print(result)
-    print("--------------------------------")
+    print(get_cluster_dict(clusterer.labels_))
+
+    reject_list = set()
+    for k, v in get_cluster_dict(clusterer.labels_).items():
+        if v < GROUP_MESSAGE_LIMIT:
+            print("reject by message limit")
+            reject_list.add(k)
+
+    for k, v in get_user_list(clusterer.labels_, userList, reject_list).items():
+        # print(len(v))
+        if len(v) <= GROUP_MEMBER_LIMIT[0] or len(v) >= GROUP_MEMBER_LIMIT[1]:
+            print("reject by member limit")
+            reject_list.add(k)
+
+    reject_list = list(reject_list)
+    print(reject_list)
+
+    result = data_processing_for_get_content(
+        np.flip(clusterer.labels_), np.flip(sentences), reject_list)
+
     final = {}
+
     for k, v in result.items():
         final[k] = {
-            "name" : get_classifies(v)
+            "name": get_classifies(v)
         }
-    
-    user_list = get_user_list(clusterer.labels_, userList)
-    for k,v in user_list.items():
+
+    user_list = get_user_list(clusterer.labels_, userList, reject_list)
+    for k, v in user_list.items():
         final[k]["userList"] = list(v)
-        
-    my_dict_converted = {
-        str(k): v for k, v in get_cluster_dict(clusterer.labels_).items()}
-    
+
     my_dict_converted2 = {
         str(k): v for k, v in final.items()}
     print(my_dict_converted2)
-    json_string = json.dumps(my_dict_converted)
     requests.post(f'http://localhost:4000/api/notification/new', json={
         'conversationId': conversation_id,
         'data': json.dumps(my_dict_converted2)
